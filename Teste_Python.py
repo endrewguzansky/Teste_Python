664c90368fd21583e475f17af109fab30bcf5e3e
Endrew Guzansky <104228384+endrewguzansky@users.noreply.github.com>
2024-08-21 23:41:15 -0300

    Update Teste_Python.py

--- Teste_Python.py
+++ Teste_Python.py
@@ -1,15 +1,15 @@
 # Definição de Classe e Instanciação
 
 class Livro:
-    def __init__(self, titulo, autor):       # INICIALIZA A FUNÇÃO 
+    def __init__(self, titulo, autor):       # INIT ~> INICIALIZAR A FUNÇÃO 
         self._titulo = titulo                # ENCAPSULAMENTO - DEIXAR PRIVADO OS OS ATRIBUTOS UTILIZANDO O (_)
         self._autor = autor
         
-    def get_titulo(self):
+    def get_titulo(self):                    # METODO GET E SET PARA PERMITIR A LEITURA E ALTERAÇÃO DESSES VALORES DE FORMA CONTROLADA.
         return self._titulo
 
     def set_titulo(self):                    
-        return self._titulo                  # METODO GET E SET PARA PERMITIR A LEITURA E ALTERAÇÃO DESSES VALORES DE FORMA CONTROLADA.
+        return self._titulo                  
                                            
     def get_autor(self):
         return self._autor
@@ -20,20 +20,20 @@ class Livro:
     def descrever(self):
         print(f"O livro {self._titulo} foi escrito por {self._autor}.")
 
-livro = Livro("As Crônicas de Gelo e Fogo","George R. R. Martin")
+livro = Livro("As Crônicas de Gelo e Fogo","George R. R. Martin")                   # INSTANCIAR OBJETO - CRIAR VARIAVEL E PASSA A CLASSE LIVRO
 livro.descrever()
 
 # HERANÇA
 
 class Ebook(Livro):
     def __init__(self, titulo, autor, formato):
-        super().__init__(titulo, autor)     # O super chama o construtor init da classe livro, que herda os atributos.
+        super().__init__(titulo, autor)                                             # O super chama o construtor init da classe livro, que herda os atributos.
         self.formato = formato
     
     def descrever(self):
         print(f"O livro {self._titulo} foi escrito por {self._autor}, no formato {self.formato}")
 
-ebook = Ebook("As Crônicas de Gelo e Fogo","George R. R. Martin",".PDF")
+ebook = Ebook("As Crônicas de Gelo e Fogo","George R. R. Martin",".PDF")             # INSTANCIAR OBJETO - CRIAR VARIAVEL E PASSA A CLASSE LIVRO
 ebook.descrever()
 
 def imprimir_descricao(livro):
